34e5f877c57060fa1cc5ba3580583fe125d06e02
Maciek <mac.w.brzezinski@gmail.com>
2017-05-26 09:59:41 +0200

    Golfer Hitbox + Golf Ball trigger collision via Animator; Project Folder structure cleanup

---
+++ Assets/scripts/GolfHit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfHit : MonoBehaviour {
+
+    public string player;
+
+    public float powerGround;
+    public float powerAir;
+
+    public Transform vectorG;
+    public Transform vectorA;
+
+    Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetButtonDown(player + "Fire1"))
+        {
+            anim.SetTrigger("Attack");
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<GolfBall>())
+        {
+            Debug.Log("GolfBall Hit!");
+        }
+    }
+}
