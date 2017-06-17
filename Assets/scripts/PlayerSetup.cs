c9c145df7ee2155808faef9c5909741fe2b0ae18
Maciek <mac.w.brzezinski@gmail.com>
2017-06-17 15:30:48 +0200

    player setup part 1

---
+++ Assets/scripts/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour {
+
+    public Controls controls;
+    public GolfHit gh;
+    public HitablePlayer hp;
+
+    public string player;
+    public int teamNo;
+
+    private void Start()
+    {
+        controls.player = player;
+        gh.player = player;
+        gh.teamNo = teamNo;
+        hp.teamNo = teamNo;
+    }
+}
--- Assets/scripts/TPPMovement.cs
+++ Assets/scripts/TPPMovement.cs
@@ -156,7 +156,7 @@ public class TPPMovement : MonoBehaviour {
         Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
         m_Rigidbody.AddForce(extraGravityForce);
 
-        //m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+        m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
     }
 
 
@@ -169,7 +169,7 @@ public class TPPMovement : MonoBehaviour {
             m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
             m_IsGrounded = false;
             m_Animator.applyRootMotion = false;
-            //m_GroundCheckDistance = 0.1f;
+            m_GroundCheckDistance = 0.1f;
         }
     }
 
